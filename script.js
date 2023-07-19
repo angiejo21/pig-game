a4463357776b18345a814546fd665cd287498a45
Angela Bellò <angela.bello90@gmail.com>
2023-07-19 18:41:04 +0200

    _game logic: initiation function

--- script.js
+++ script.js
@@ -14,9 +14,18 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+
+// const scores = [0, 0]; //Punti finali
+// let currentScore = 0;
+// let activePlayer = 0;
+// let isPlaying = true;
+
+let scores, currentScore, activePlayer, isPlaying;
+
+init();
 
 function switchPlayer() {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -26,10 +35,27 @@ function switchPlayer() {
   player1EL.classList.toggle('player--active');
 }
 
-const scores = [0, 0]; //Punti finali
-let currentScore = 0;
-let activePlayer = 0;
-let isPlaying = true;
+function init() {
+  //eliminare classe winner
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  //settare l'active player e aggiungere la classe active
+  activePlayer = 0;
+  player0EL.classList.add('player--active');
+  //Azzerare scores
+  scores = [0, 0];
+  //Azzerare il current score
+  currentScore = 0;
+  //Azzerare punteggi finali e correnti
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  //nascondere dado
+  diceEl.classList.add('hidden');
+  // isplaying true
+  isPlaying = true;
+}
 
 //Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -59,7 +85,7 @@ btnHold.addEventListener('click', () => {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2.Check if player's score is >= 100
-    if (scores[activePlayer] >= 100) {
+    if (scores[activePlayer] >= 20) {
       //Finish game
       isPlaying = false;
       diceEl.classList.add('hidden');
@@ -75,3 +101,5 @@ btnHold.addEventListener('click', () => {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
